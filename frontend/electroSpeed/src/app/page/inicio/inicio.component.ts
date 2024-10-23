015ddb4ece8189a485794d0d855ba298e2eec6da
GLa2s <adricarmonagalvez@gmail.com>
2024-10-23 17:57:46 +0200

    inicio verde y organización
    poniendo 2 los primeros apartados del inicio y organización de carpetas

--- frontend/electroSpeed/src/app/page/inicio/inicio.component.ts
+++ frontend/electroSpeed/src/app/page/inicio/inicio.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TituloComponent } from "./titulo/titulo.component";
+import { OfrecerMovilidadComponent } from "./ofrecer-movilidad/ofrecer-movilidad.component";
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [TituloComponent, OfrecerMovilidadComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
---
+++ frontend/electroSpeed/src/app/page/inicio/ofrecer-movilidad/ofrecer-movilidad.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ofrecer-movilidad',
+  standalone: true,
+  imports: [],
+  templateUrl: './ofrecer-movilidad.component.html',
+  styleUrl: './ofrecer-movilidad.component.css'
+})
+export class OfrecerMovilidadComponent {
+
+}
