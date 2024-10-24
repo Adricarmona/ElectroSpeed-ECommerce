59ca16cd0388837a09559684f852cca0510177e4
GLa2s <129745244+Adricarmona@users.noreply.github.com>
2024-10-24 22:14:19 +0200

    arreglando destacados

--- frontend/electroSpeed/src/app/pages/inicio/inicio.component.ts
+++ frontend/electroSpeed/src/app/pages/inicio/inicio.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { TituloComponent } from "./titulo/titulo.component";
 import { OfrecerMovilidadComponent } from "./ofrecer-movilidad/ofrecer-movilidad.component";
-import { DestacadosComponent } from "./destacados/destacados.component";
+import { DestacadosComponent } from '../destacados/destacados.component';
 
 @Component({
   selector: 'app-inicio',
