7ec9b9b5c9624c298223b9400f577675b6033a01
GLa2s <129745244+Adricarmona@users.noreply.github.com>
2024-10-25 01:14:40 +0200

    juntando el nav

--- frontend/electroSpeed/src/app/app.component.ts
+++ frontend/electroSpeed/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InicioComponent } from "./pages/inicio/inicio.component";
 import { BaseLoginRegistroComponent } from './pages/base-login-registro/base-login-registro.component';
+import { NavbarComponent } from "./page/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
--- frontend/electroSpeed/src/app/pages/inicio/inicio.component.ts
+++ frontend/electroSpeed/src/app/pages/inicio/inicio.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { TituloComponent } from "./titulo/titulo.component";
 import { OfrecerMovilidadComponent } from "./ofrecer-movilidad/ofrecer-movilidad.component";
 import { DestacadosComponent } from '../destacados/destacados.component';
+import { NavbarComponent } from "../../page/navbar/navbar.component";
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [TituloComponent, OfrecerMovilidadComponent, DestacadosComponent],
+  imports: [TituloComponent, OfrecerMovilidadComponent, DestacadosComponent, NavbarComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
