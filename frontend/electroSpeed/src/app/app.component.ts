836add90341d519ea7c2ac2fc50054b0298ae647
GLa2s <129745244+Adricarmona@users.noreply.github.com>
2024-10-25 02:30:05 +0200

    pestañas servicios funcional

--- frontend/electroSpeed/src/app/app.component.ts
+++ frontend/electroSpeed/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { InicioComponent } from "./pages/inicio/inicio.component";
 import { BaseLoginRegistroComponent } from './pages/base-login-registro/base-login-registro.component';
 import { NavbarComponent } from "./page/navbar/navbar.component";
+import { ServiciosComponent } from "./pages/inicio/servicios/servicios.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InicioComponent, BaseLoginRegistroComponent],
+  imports: [RouterOutlet, InicioComponent, BaseLoginRegistroComponent, NavbarComponent, ServiciosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/electroSpeed/src/app/pages/inicio/inicio.component.ts
+++ frontend/electroSpeed/src/app/pages/inicio/inicio.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { TituloComponent } from "./titulo/titulo.component";
 import { OfrecerMovilidadComponent } from "./ofrecer-movilidad/ofrecer-movilidad.component";
-import { DestacadosComponent } from '../destacados/destacados.component';
+import { DestacadosComponent } from '../inicio/destacados/destacados.component';
 import { NavbarComponent } from "../../page/navbar/navbar.component";
+import { ServiciosComponent } from "./servicios/servicios.component";
+import { MarcasComponent } from "./marcas/marcas.component";
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [TituloComponent, OfrecerMovilidadComponent, DestacadosComponent, NavbarComponent],
+  imports: [TituloComponent, OfrecerMovilidadComponent, DestacadosComponent, NavbarComponent, ServiciosComponent, MarcasComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
