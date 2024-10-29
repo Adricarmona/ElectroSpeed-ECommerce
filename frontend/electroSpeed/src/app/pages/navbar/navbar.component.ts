35264d542c1303f70ff03eefb03e60e56d4d5d88
GLa2s <129745244+Adricarmona@users.noreply.github.com>
2024-10-29 18:46:47 +0100

    inicio de desplegable navbar

--- frontend/electroSpeed/src/app/pages/navbar/navbar.component.ts
+++ frontend/electroSpeed/src/app/pages/navbar/navbar.component.ts
@@ -1,3 +1,5 @@
+import { query } from '@angular/animations';
+import { Block } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,4 +11,15 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  desplegable(){
+    const desplegable = document.getElementById("desplegableUsuarios")
+    if (desplegable != null) {
+      const displayStyle = window.getComputedStyle(desplegable).display;
+      if (displayStyle == "none") {
+        desplegable.style.display = "block"
+      } else if(displayStyle == "block") {
+        desplegable.style.display = "none"
+      }
+    }
+  }
 }
