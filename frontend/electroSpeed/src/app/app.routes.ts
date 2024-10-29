1cca3b72c769d01deab0b788e4a263f0f79d3805
noee1410 <noegrelaud@gmail.com>
2024-10-29 18:50:31 +0100

    Creacioin de la nueva vista del login y registro

--- frontend/electroSpeed/src/app/app.routes.ts
+++ frontend/electroSpeed/src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { InicioComponent } from './pages/inicio/inicio.component';
  import { BaseLoginRegistroComponent } from './pages/base-login-registro/base-login-registro.component'; 
 import { LoginComponent } from './pages/base-login-registro/login/login.component';
 import { RegistroComponent } from './pages/base-login-registro/registro/registro.component';
+import { Component } from '@angular/core';
+import { LoginRegistroComponent } from './pages/login-registro/login-registro.component';
 
 export const routes: Routes = [
     {path: '', component: InicioComponent},
-     {path: 'f', component: BaseLoginRegistroComponent}, 
+    {path: 'f', component: BaseLoginRegistroComponent}, 
     {path: 'login', component: LoginComponent},
-    {path: 'registro', component: RegistroComponent}
+    {path: 'registro', component: RegistroComponent},
+    {path: 'logreg', component: LoginRegistroComponent},
 ];
