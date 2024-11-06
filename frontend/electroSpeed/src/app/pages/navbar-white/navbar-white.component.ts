b4b5d6419e23b34c3b7ec799aff2cf898f9b8e52
GLa2s <129745244+Adricarmona@users.noreply.github.com>
2024-11-06 19:32:34 +0100

    arreglando lo que hay

--- frontend/electroSpeed/src/app/pages/navbar-white/navbar-white.component.ts
+++ frontend/electroSpeed/src/app/pages/navbar-white/navbar-white.component.ts
@@ -1,12 +1,46 @@
+import { query } from '@angular/animations';
+import { Block } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { AuthService } from '../../service/auth.service'
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'app-navbar-white',
   standalone: true,
   imports: [],
   templateUrl: './navbar-white.component.html',
   styleUrl: './navbar-white.component.css'
 })
 export class NavbarWhiteComponent {
+  constructor(private authService: AuthService) {}
 
+  /* cogemos el token para ver si existe o quien es */
+ usuarioToken() {
+    const token = this.authService.getToken()
+    return token
+ }
+ 
+ vaciarToken() {
+    this.authService.setTokenLocal("")
+    this.authService.setTokenSesion("")
+    location.reload()
+ }
+
+ nombreToken() {
+  const nombreNavBar = document.getElementById("nombreUsuario")
+  if (nombreNavBar) {
+    nombreNavBar.innerText = this.authService.getNameUserToken()
+  }
+ }
+
+  desplegable(){
+    const desplegable = document.getElementById("desplegableUsuarios")
+    if (desplegable != null) {
+      const displayStyle = window.getComputedStyle(desplegable).display;
+      if (displayStyle == "none") {
+        desplegable.style.display = "block"
+      } else if(displayStyle == "block") {
+        desplegable.style.display = "none"
+      }
+    }
+  }
 }
