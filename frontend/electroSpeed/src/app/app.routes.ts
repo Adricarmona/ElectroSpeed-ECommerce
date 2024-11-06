2cd07d853f24cb222e45cac2b04f9457ef45f385
GLa2s <129745244+Adricarmona@users.noreply.github.com>
2024-11-06 02:45:16 +0100

    haciendo que funcione

--- frontend/electroSpeed/src/app/app.routes.ts
+++ frontend/electroSpeed/src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
- import { BaseLoginRegistroComponent } from './pages/base-login-registro/base-login-registro.component'; 
+import { BaseLoginRegistroComponent } from './pages/base-login-registro/base-login-registro.component'; 
 import { LoginComponent } from './pages/base-login-registro/login/login.component';
 import { RegistroComponent } from './pages/base-login-registro/registro/registro.component';
+import { SobreNosotrosComponent } from './pages/sobre-nosotros/sobre-nosotros.component';
 
 export const routes: Routes = [
     {path: '', component: InicioComponent},
      {path: 'f', component: BaseLoginRegistroComponent}, 
     {path: 'login', component: LoginComponent},
-    {path: 'registro', component: RegistroComponent}
+    {path: 'registro', component: RegistroComponent},
+    {path: 'sobreNosotros', component: SobreNosotrosComponent}
 ];
