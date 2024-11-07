eb2160e8f502142b24fed390a64652005c4ed798
GLa2s <129745244+Adricarmona@users.noreply.github.com>
2024-11-07 20:22:09 +0100

    cambiando el grosor y el color del navbar y arreglando lo del buscador

--- frontend/electroSpeed/src/app/pages/sobre-nosotros/sobre-nosotros.component.ts
+++ frontend/electroSpeed/src/app/pages/sobre-nosotros/sobre-nosotros.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { NavbarWhiteComponent } from '../navbar-white/navbar-white.component';
 import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-sobre-nosotros',
   standalone: true,
-  imports: [NavbarWhiteComponent, FooterComponent],
+  imports: [FooterComponent],
   templateUrl: './sobre-nosotros.component.html',
   styleUrl: './sobre-nosotros.component.css'
 })
