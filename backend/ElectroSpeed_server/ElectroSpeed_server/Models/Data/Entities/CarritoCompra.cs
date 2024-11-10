73fb84f92c2c14854c69bec02ac5f8664e956d9a
HectorDominguezG <hdomgar0208@g.educaand.es>
2024-11-10 17:22:34 +0100

    me faltaba esto del commit anterior

--- backend/ElectroSpeed_server/ElectroSpeed_server/Models/Data/Entities/CarritoCompra.cs
+++ backend/ElectroSpeed_server/ElectroSpeed_server/Models/Data/Entities/CarritoCompra.cs
@@ -1,7 +1,17 @@
-﻿namespace ElectroSpeed_server.Models.Data.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ElectroSpeed_server.Models.Data.Entities
 {
     public class CarritoCompra
     {
         public int Id { get; set; }
+        public int BicicletasId { get; set; }
+        public int? UsuariosId { get; set; }
+
+        [ForeignKey(nameof(BicicletasId))]
+        public Bicicletas Bicletas { get; set; }
+
+        [ForeignKey(nameof(UsuariosId))]
+        public Usuarios Usuarios { get; set; }
     }
 }
