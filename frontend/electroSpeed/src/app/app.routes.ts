7d15f076b68654dfe3e5f02e07b4033e6abf55a4
GLa2s <129745244+Adricarmona@users.noreply.github.com>
2024-11-10 18:36:46 +0100

    maquetado destacados

--- frontend/electroSpeed/src/app/app.routes.ts
+++ frontend/electroSpeed/src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { InicioComponent } from './pages/inicio/inicio.component';
 import { LoginRegistroComponent } from './pages/login-registro/login-registro.component';
 import { CatalogoComponent } from './pages/catalogo/catalogo.component';
 import { SobreNosotrosComponent } from './pages/sobre-nosotros/sobre-nosotros.component';
+import { VistaDetalleComponent } from './pages/vista-detalle/vista-detalle.component';
 
 export const routes: Routes = [
     {path: '', component: InicioComponent},
     {path: 'logreg', component: LoginRegistroComponent},
     {path: 'catalogo', component: CatalogoComponent},
-    {path: 'catalogo/:id', component: CatalogoComponent},
+    {path: 'detalle', component: VistaDetalleComponent},
     {path: 'sobreNosotros', component:  SobreNosotrosComponent}
 ];
---
+++ frontend/electroSpeed/src/app/pages/vista-detalle/vista-detalle.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vista-detalle',
+  standalone: true,
+  imports: [],
+  templateUrl: './vista-detalle.component.html',
+  styleUrl: './vista-detalle.component.css'
+})
+export class VistaDetalleComponent {
+
+}
