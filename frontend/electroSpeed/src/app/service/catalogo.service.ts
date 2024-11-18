54b4c525d8e5c5185b2b7f21480c2d980d0e681c
HectorDominguezG <hdomgar0208@g.educaand.es>
2024-11-18 17:23:21 +0100

    blockchain (no tengo ni idea de que he hecho)

--- frontend/electroSpeed/src/app/service/catalogo.service.ts
+++ frontend/electroSpeed/src/app/service/catalogo.service.ts
@@ -30,7 +30,7 @@ export class CatalogoService {
   }
 
 
-  async showOneBike(id: number){
+  async showOneBike(id: string){
     try {
       const request: Observable<Bicicletas> = this.http.get<Bicicletas>(`${this.BASE_URL}bici/${id}`);
       const result: Bicicletas = await lastValueFrom(request);
