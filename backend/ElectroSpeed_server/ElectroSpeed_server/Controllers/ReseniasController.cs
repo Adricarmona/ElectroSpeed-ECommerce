b3c6517be0766258cd204fe093ffe5c3a6d35fa5
noee1410 <noegrelaud@gmail.com>
2024-11-20 18:47:54 +0100

    Tonteria de numeros de adrian

--- backend/ElectroSpeed_server/ElectroSpeed_server/Controllers/ReseniasController.cs
+++ backend/ElectroSpeed_server/ElectroSpeed_server/Controllers/ReseniasController.cs
@@ -62,7 +62,7 @@ namespace ElectroSpeed_server.Controllers
             {
                 Id = model.Id,
                 textoDeResenia = model.texto,
-                resultadoResenia = (int)output.PredictedLabel,
+                resultadoResenia = output.PredictedLabel+2,
                 UsuarioId = model.UsuarioId,
                 BicicletaId = model.BicicletaId
             };
