e91f14ded72700be82c2838f832a3b2a3d4747a1
noee1410 <noegrelaud@gmail.com>
2024-11-25 13:40:46 +0100

    s

--- backend/ElectroSpeed_server/ElectroSpeed_server/Controllers/CheckoutController.cs
+++ backend/ElectroSpeed_server/ElectroSpeed_server/Controllers/CheckoutController.cs
@@ -26,49 +26,49 @@ namespace ElectroSpeed_server.Controllers
             return bici;
         }
 
-        //[HttpGet("embedded")]
-        //public async Task<ActionResult> EmbededCheckout(int idUsuario)
-        //{
-        //    CheckoutTarjeta checkout = new CheckoutTarjeta(_esContext);
+        [HttpGet("embedded")]
+        public async Task<ActionResult> EmbededCheckout(int idUsuario)
+        {
+            CheckoutTarjeta checkout = new CheckoutTarjeta(_esContext);
 
-        //    var orden = checkout.Ordentemporal(idUsuario);
+            var orden = checkout.Ordentemporal(idUsuario);
 
-        //    var lineItems = new List<SessionLineItemOptions>();
+            var lineItems = new List<SessionLineItemOptions>();
 
-        //    foreach (var b in orden.BicisCantidad)
-        //    {
-        //        var bici = _esContext.Bicicletas.FirstOrDefault(r => r.Id == b.IdBici);
-        //            lineItems.Add(new SessionLineItemOptions()
-        //            {
-        //                PriceData = new SessionLineItemPriceDataOptions()
-        //                {
-        //                    Currency = "eur",
-        //                    UnitAmount = (long)(1 * 100),
-        //                    ProductData = new SessionLineItemPriceDataProductDataOptions()
-        //                    {
-        //                        Name = bici.MarcaModelo,
-        //                        Description = bici.Descripcion,
-        //                        Images = new List<string> { bici.UrlImg }
-        //                    }
-        //                },
-        //                Quantity = b.cantidad
-        //            });
-        //    }
+            foreach (var b in orden.BicisCantidad)
+            {
+                var bici = _esContext.Bicicletas.FirstOrDefault(r => r.Id == b.IdBici);
+                lineItems.Add(new SessionLineItemOptions()
+                {
+                    PriceData = new SessionLineItemPriceDataOptions()
+                    {
+                        Currency = "eur",
+                        UnitAmount = (long)(1 * 100),
+                        ProductData = new SessionLineItemPriceDataProductDataOptions()
+                        {
+                            Name = bici.MarcaModelo,
+                            Description = bici.Descripcion,
+                            Images = new List<string> { bici.UrlImg }
+                        }
+                    },
+                    Quantity = b.cantidad
+                });
+            }
 
-        //    SessionCreateOptions options = new SessionCreateOptions
-        //    {
-        //        UiMode = "embedded",
-        //        Mode = "payment",
-        //        PaymentMethodTypes = ["card"],
-        //        LineItems = lineItems,
-        //        CustomerEmail = "correo_cliente@correo.es",
-        //    };
+            SessionCreateOptions options = new SessionCreateOptions
+            {
+                UiMode = "embedded",
+                Mode = "payment",
+                PaymentMethodTypes = ["card"],
+                LineItems = lineItems,
+                CustomerEmail = "correo_cliente@correo.es",
+            };
 
-        //    SessionService service = new SessionService();
-        //    Session session = await service.CreateAsync(options);
+            SessionService service = new SessionService();
+            Session session = await service.CreateAsync(options);
 
-        //    return Ok(new { clientSecret = session.ClientSecret });
-        //}
+            return Ok(new { clientSecret = session.ClientSecret });
+        }
 
         [HttpGet("status/{sessionId}")]
         public async Task<ActionResult> SessionStatus(string sessionId)
