c844a62af972e749e60a24c30c3af7d38b58d793
HectorDominguezG <hdomgar0208@g.educaand.es>
2024-11-27 20:36:02 +0100

    blockhain empezado (se viene)

--- frontend/electroSpeed/src/app/app.component.ts
+++ frontend/electroSpeed/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { NavbarComponent } from './pages/navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InicioComponent,NavbarComponent],
+  imports: [RouterOutlet, InicioComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -15,11 +15,11 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     console.log(window.ethereum);
 
-    }
-    }
-    declare global {
-    interface Window {
+  }
+}
+declare global {
+  interface Window {
     ethereum: any;
-    }
+  }
 }
 
