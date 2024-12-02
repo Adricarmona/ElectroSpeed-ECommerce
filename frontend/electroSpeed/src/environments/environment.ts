42fe424c171fa1b2269a9186377ea91180ca07c3
GLa2s <129745244+Adricarmona@users.noreply.github.com>
2024-12-02 18:08:19 +0100

    una linea

--- frontend/electroSpeed/src/environments/environment.ts
+++ frontend/electroSpeed/src/environments/environment.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+    apiUrl: 'https://electrospeeddam.runasp.net/',
+    stripePublicKey: 'pk_test_51QJzj7FTox3Yp5UyayFVvp2MycY7lWlxMU1aoi0HnAwUP6T9ZgmAQvZPI33LL8qtvpD8QiWQNmsKtZrt0uOWrAww00s7zBzWGN'
+};
