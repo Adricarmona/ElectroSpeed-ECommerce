641edef476f067c551d7f2932d68b3d209e24041
GLa2s <129745244+Adricarmona@users.noreply.github.com>
2024-12-07 00:06:20 +0100

    vista de administrador front

--- frontend/electroSpeed/src/app/pages/administrador/administrador.component.ts
+++ frontend/electroSpeed/src/app/pages/administrador/administrador.component.ts
@@ -13,8 +13,6 @@ import { FormsModule } from '@angular/forms';
 })
 export class AdministradorComponent {
 
-  boolean: boolean = true
-
   // bicicleta
   bicicletaSeleccionada: Bicicletas = {
     id: 0,
@@ -28,55 +26,48 @@ export class AdministradorComponent {
 
   // usuario
   usuarioSeleccionado: Usuarios = {
-    id: 1,
-    name: 'nombre',
-    username: 'usuario',
-    email: 'correo',
-    rol: true
+    id: 0,
+    name: '',
+    username: '',
+    email: '',
+    rol: false
   }
+
+
 
   // bicicletas
   bicicletasTotales: Bicicletas[] = [{
-    id: 0,
-    marcaModelo: '',
-    descripcion: '',
-    precio: 0,
-    stock: 0,
+    id: 1,
+    marcaModelo: 'marcaModelo',
+    descripcion: 'descripcion',
+    precio: 1,
+    stock: 1,
     urlImg: 'administrador/chinoEnBici.gif',
     cantidad: 0
   },
   {
-    id: 0,
-    marcaModelo: '',
-    descripcion: '',
-    precio: 0,
-    stock: 0,
+    id: 2,
+    marcaModelo: 'marcaModelo1',
+    descripcion: 'descripcion1',
+    precio: 2,
+    stock: 2,
+    urlImg: 'administrador/chinoEnBici.gif',
+    cantidad: 0
+  },
+  {
+    id: 3,
+    marcaModelo: 'marcaModelo2',
+    descripcion: 'descripcion2',
+    precio: 3,
+    stock: 3,
     urlImg: 'administrador/chinoEnBici.gif',
     cantidad: 0
   }]
 
-  bicicletasFiltradas: Bicicletas[] = [{
-    id: 0,
-    marcaModelo: 'marcaModelo',
-    descripcion: '',
-    precio: 0,
-    stock: 0,
-    urlImg: 'administrador/chinoEnBici.gif',
-    cantidad: 0
-  },
-  {
-    id: 0,
-    marcaModelo: '',
-    descripcion: '',
-    precio: 0,
-    stock: 0,
-    urlImg: 'administrador/chinoEnBici.gif',
-    cantidad: 0
-  }]
+  bicicletasFiltradas: Bicicletas[]
 
 
   // usuarios
-
   usuariosTotales: Usuarios[] = [{
     id: 1,
     name: 'nombre',
@@ -86,36 +77,103 @@ export class AdministradorComponent {
   },
   {
     id: 2,
-    name: 'nombre',
-    username: 'usuario',
-    email: 'correo',
+    name: 'nombre1',
+    username: 'usuario1',
+    email: 'correo1',
     rol: true
-  }]
-
-  usuariosFiltrados: Usuarios[] = [{
-    id: 1,
-    name: 'nombre',
-    username: 'usuario',
-    email: 'correo',
-    rol: false
   },
   {
-    id: 2,
-    name: 'nombre',
-    username: 'usuario',
-    email: 'correo',
+    id: 3,
+    name: 'nombre2',
+    username: 'usuario2',
+    email: 'correo2',
     rol: true
   }]
 
+  usuariosFiltrados: Usuarios[]
+
     // usuario o bici
-    usuarioOBici: number = 0 // 0 usuario , 1 bici
-    seleccionado: number = 0 // si clica en un usuario o bici (como arriba)
+    usuarioOBici: number = 1 // 0 usuario , 1 bici
+    seleccionado: number = 1 // si clica en un usuario o bici (como arriba)
+
+    // id par menu
+    idBuscarMenu: number = 1
+
+  ngOnInit(): void {
+    this.sumarRestarEjecutar(0)
+  }
 
   cambiarUsuarioBici(){
-    if (this.usuarioOBici == 1) {
+    if (this.usuarioOBici == 1) { // si es bici
       this.usuarioOBici = 0
     } else {
       this.usuarioOBici = 1
     }
+
+    this.sumarRestarEjecutar(0)
   }
+
+  SeleccionadorBicisUsuarios() {
+    if (this.usuarioOBici == 1) { // si es bici
+
+      this.bicicletasFiltradas = []
+
+      this.bicicletasTotales.forEach(bici => {
+        if (bici.id == this.idBuscarMenu || bici.id == (this.idBuscarMenu + 1)) {
+          this.bicicletasFiltradas.push(bici)
+        }
+      });
+
+    } else {
+
+      this.usuariosFiltrados = []
+
+      this.usuariosTotales.forEach(bici => {
+        if (bici.id == this.idBuscarMenu || bici.id == (this.idBuscarMenu + 1)) {
+          this.usuariosFiltrados.push(bici)
+        }
+      });
+
+    }
+  }
+
+  sumarRestarEjecutar(number: number) {
+
+    this.idBuscarMenu = this.idBuscarMenu + number
+    
+    if (this.idBuscarMenu < 1) {
+      this.idBuscarMenu = 1
+    }
+
+    this.SeleccionadorBicisUsuarios()
+  }
+
+  seleccionarDestacado(idSeleccionado: number, seleccionado: number) {
+
+    this.seleccionado = seleccionado;
+
+    if (this.seleccionado == 0) { // 0 es usuario y 1 es bici
+
+      this.usuariosFiltrados.forEach(usuario =>{
+
+        if(idSeleccionado == usuario.id) {
+          this.usuarioSeleccionado = usuario
+        }
+
+      })
+
+    } else {
+      
+      this.bicicletasFiltradas.forEach(bici =>{
+
+        if(idSeleccionado == bici.id) {
+          this.bicicletaSeleccionada = bici
+        }
+
+      })
+
+    }
+
+  }
+
 }
