78f9b6229154596d9caa3314b51abf9020cba424
HectorDominguezG <hdomgar0208@g.educaand.es>
2024-12-11 12:27:07 +0100

    dsda

--- backend/ElectroSpeed_server/ElectroSpeed_server/Models/Data/Dto/Correo.cs
+++ backend/ElectroSpeed_server/ElectroSpeed_server/Models/Data/Dto/Correo.cs
@@ -2,5 +2,9 @@
 {
     public class Correo
     {
+        public string to { get; set; }
+        public string subject { get; set; }
+        public string body { get; set; }
+        public bool ishtml { get; set; }
     }
 }
